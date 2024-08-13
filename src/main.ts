d455c2c67716086657921d8add1e863e8a7d20c6
Darya <daryaleys@ya.ru>
2024-08-13 18:44:03 +0300

    remove gsap and touch event fix

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,34 @@
 import Swiper from "swiper";
 import { Mousewheel, Parallax } from "swiper/modules";
 import "swiper/css";
-
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
 import "./style/main.scss";
-
-gsap.registerPlugin(ScrollTrigger);
 
 const contentElements = document.querySelectorAll(".content");
 contentElements.forEach((content) => {
-  const word = content.querySelector(".letters") as HTMLElement;
-  if (!word) return;
+    const word = content.querySelector(".letters") as HTMLElement;
+    if (!word) return;
 
-  const chars: string[] | undefined = word.textContent?.split("");
-  if (chars && chars.length) {
-    chars.forEach((char, index) => {
-      const span = document.createElement("span");
-      span.innerText = char;
-      span.classList.add('letter');
-      span.style.zIndex = (chars.length - index).toString();
-      span.style.transitionDelay = `${index * 0.1}s`;
-      content.append(span);
-    })
-  }
+    const chars: string[] | undefined = word.textContent?.split("");
+    if (chars && chars.length) {
+        chars.forEach((char, index) => {
+            const span = document.createElement("span");
+            span.innerText = char;
+            span.classList.add("letter");
+            span.style.zIndex = (chars.length - index).toString();
+            span.style.transitionDelay = `${index * 0.1}s`;
+            content.append(span);
+        });
+    }
 
-  content.removeChild(word);
-})
-
-window.addEventListener('DOMContentLoaded', () => {
-  new Swiper(".slider", {
-    modules: [Mousewheel, Parallax],
-    direction: "vertical",
-    mousewheel: true,
-    speed: 1500,
-    parallax: true,
+    content.removeChild(word);
 });
-})
+
+window.addEventListener("DOMContentLoaded", () => {
+    new Swiper(".slider", {
+        modules: [Mousewheel, Parallax],
+        direction: "vertical",
+        mousewheel: true,
+        speed: 1500,
+        parallax: true,
+    });
+});
